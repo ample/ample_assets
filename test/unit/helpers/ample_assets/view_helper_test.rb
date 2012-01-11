ec1a345605f10a354497c4255cce4db3aef49795
Bobby Uhlenbrock <bobby@helloample.com>
2012-01-11 16:57:10 -0500

    Moved image_asset method and associated tests into gem

---
+++ test/unit/helpers/ample_assets/view_helper_test.rb
@@ -0,0 +1,116 @@
+require 'test_helper'
+
+module AmpleAssets
+  class ViewHelperTest < ActionView::TestCase
+    
+    # TODO: Routes are not loaded in this context, so this test fails.
+    # context 'Assets Toolbar' do
+    #   
+    #   setup do
+    #     render :text => assets_toolbar
+    #   end
+    # 
+    #   should 'set the mout_at path' do
+    #     assert output_buffer.include? AmpleAssets::Engine.config.mount_at
+    #   end
+    # 
+    # end
+    
+    context 'Image Asset' do
+      
+      setup do
+        @file = File.create :attachment => ::File.read("#{Rails.root}/app/assets/images/rails.png")
+        @page = Page.create :title => 'Test Page', :file_id => @file.id
+      end
+      
+      context 'without args' do
+
+        setup do
+          render :text => image_asset(@page)
+        end
+        
+        should 'not require dimensions to be set' do
+          assert_select("img[width=50]")
+          assert_select("img[height=64]")
+        end
+
+      end
+      
+      context 'with some args' do
+        
+        setup do
+          render :text => image_asset(@page, { :size => false })
+        end
+        
+        should 'have no dimensions if size is false' do
+          assert_select("img[width]", false)
+          assert_select("img[height]", false)
+        end
+
+      end
+      
+      context 'with args' do
+        
+        setup do
+          render :text => image_asset(@page, {
+                            :dimensions => '50x50#', 
+                            :style => 'border: 1px solid;', 
+                            :class => "some-selector", 
+                            :title => @page.title,
+                            :link => 'http://yahoo.com',
+                            :video => true,
+                            :video_dimensions => '1x1',
+                            :encode => :jpg })
+        end
+        
+        should 'output an img tag' do
+          assert_select("img")
+        end
+        
+        should 'have an alt attribute' do
+          assert_select("img[alt]")
+        end
+        
+        should 'not contain invalid attributes' do
+          assert_select("img[dimensions]", false)
+          assert_select("img[encode]", false)
+          assert_select("img[object]", false)
+          assert_select("img[video_dimensions]", false)
+        end
+        
+        should 'have a title attribute if one is defined' do
+          assert_select("img[title]")
+        end
+
+        should 'have a style attribute if one is defined' do
+          assert_select("img[style*=border]")
+        end
+
+        should 'have a class attribute if one is defined' do
+          assert_select("img[class*=some-selector]")
+        end
+
+        should 'have width and height attributes if dimensions are defined' do
+          assert_select("img[width=50]")
+          assert_select("img[height=50]")
+        end
+
+        should 'be wrapped in a link tag if one is defined' do
+          assert_select("a[href=http://yahoo.com]")
+        end
+
+        should 'output facebox specific attrs if :video is true' do
+          assert_select("a[rel=facebox]")
+          assert_select("a[rev=iframe|1x1]")
+        end
+
+        should 'be encoded properly if an encoding is defined' do
+          assert_select("img[src*=jpg]")
+        end
+        
+      end
+      
+    end
+    
+  end
+end

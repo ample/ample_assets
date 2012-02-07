72699bacc90d7ed50b18e642276ff16715a19b0b
Bobby Uhlenbrock <bobby@helloample.com>
2012-02-07 13:48:47 -0500

    Add conditional for serialized objects

--- lib/ample_assets/form_builder.rb
+++ lib/ample_assets/form_builder.rb
@@ -2,7 +2,11 @@ module AmpleAssets
   module FormBuilder
 
     def asset_drop(method, options = {})
-      options.merge!(:object => @object.send(method.to_s.gsub(/_id$/, '')))
+      if options.delete(:serialized)
+        options.merge!(:object => AmpleAssets::File.find(options[:value]))
+      else
+        options.merge!(:object => @object.send(method.to_s.gsub(/_id$/, '')))
+      end
       @template.asset_drop(@object_name, method, options)
     end
 

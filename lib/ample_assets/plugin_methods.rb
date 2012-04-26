4029647f908fb156272ac942044cf8f65963a62c
Bobby Uhlenbrock <bobby@helloample.com>
2012-04-26 12:50:53 -0400

    Fixed plugin helper

--- lib/ample_assets/plugin_methods.rb
+++ lib/ample_assets/plugin_methods.rb
@@ -2,7 +2,7 @@ module AmpleAssets
   module PluginMethods
     
     def has_asset(name = 'file', options = {})
-      configuration = { :foreign_key => 'file_id', :class_name => 'AmpleAssets::File' }
+      configuration = { :foreign_key => "#{name}_id", :class_name => 'AmpleAssets::File' }
       configuration.update(options) if options.is_a?(Hash)
       self.belongs_to name, configuration
     end
